959f860070a873e7991859672dff2f58b238728a
Andre Sales <asalesg@gmail.com>
2022-10-08 12:15:20 -0300

    Initial commit

---
+++ src/sintaxevariaveisfluxo/TestaCaracteres.java
@@ -0,0 +1,18 @@
+package sintaxevariaveisfluxo;
+
+public class TestaCaracteres {
+	
+	public static void main(String[] args) {
+		char letra = 'a';
+		System.out.println(letra);
+		
+		String palavra = "asuhdiuashduias sangue de jesus tem poder";
+		
+		System.out.println(palavra);
+		
+		palavra = palavra + 2020;
+		System.out.println(palavra);
+		
+	}
+
+}
